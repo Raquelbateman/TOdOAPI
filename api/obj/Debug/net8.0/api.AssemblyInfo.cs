2bce42a703fe4b10cb911e8f29fa1bec91564acf
Raquelbateman <aliciaraquel1017@gmail.com>
2024-05-01 21:18:17 -0700

    update

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+064795b5ba19c2debee9ebce95b1eec7a1361b87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3045ac30309f150fcbdd08c33f3c64d51499124")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
