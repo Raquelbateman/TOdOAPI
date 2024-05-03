e9c5ab235a12a5793aff54bd83da072db7fa5ff4
Raquelbateman <aliciaraquel1017@gmail.com>
2024-05-02 21:17:22 -0700

    update

--- api/Data/AppDbContext.cs
+++ api/Data/AppDbContext.cs
@@ -5,16 +5,15 @@ using System.Threading.Tasks;
 using api.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace api.Data
+namespace api.Data;
 
-    public class AppDbContext : DbContext
-    {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
-        {
+   public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+   {
+    
 
-        }
+    
+   }
 
-
-    public DbSet<TodoItem> TodoItems { get;}
-
-    }
+}
